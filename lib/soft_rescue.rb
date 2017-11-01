7fbf923e5f2b90b3ac33d4110229ca226cd71c82
Sergey Staskov <sergey.staskov@dellin.ru>
2017-11-01 17:04:08 +0300

    rework code according to code climate

--- lib/soft_rescue.rb
+++ lib/soft_rescue.rb
@@ -1,36 +1,62 @@
 require 'soft_rescue/version'
 
-# SoftRescue.configure do |config|
-#  config.logger = Logger.new
-#  config.enabled = Rails.env.production?
-#  config.capture_exception = -> exception { Raven.capture_exception(exception) }
-# end
-#
-# SoftRescue.call(on_failure: 0, message: "Hi")
-
 module SoftRescue
+  # SoftRescue.configure do |config|
+  #  config.logger = Logger.new
+  #  config.enabled = Rails.env.production?
+  #  config.capture_exception = -> exception { Raven.capture_exception(exception) }
+  # end
+  def self.configure
+    yield Config
+  end
+
+  # SoftRescue.call(on_failure: 0, message: "my custom exception info")
+  # SoftRescue.call(on_failure: -> { puts "fail!" }, message: "my custom exception info")
+  def self.call(on_failure: nil, message: nil)
+    yield
+  rescue StandardError => exception
+    ExceptionHandler.new(exception, on_failure, message).handle
+  end
+
+  # private logic
   module Config
     class << self
       attr_accessor :logger, :capture_exception, :enabled
     end
   end
 
-  def self.configure
-    yield Config
-  end
-
-  def self.call(on_failure: nil, message: nil)
-    yield
-  rescue StandardError => exception
-    raise exception unless Config.enabled
-
-    if Config.logger
-      message = [message, exception.message].compact.join('. ')
-      Config.logger.error(message)
+  class ExceptionHandler
+    def initialize(exception, on_failure, message)
+      @exception = exception
+      @on_failure = on_failure
+      @message = message
     end
 
-    Config.capture_exception.call(exception) if Config.capture_exception
+    def handle
+      raise @exception unless Config.enabled
+      handle_exception
+    end
 
-    on_failure.is_a?(Proc) ? on_failure.call : on_failure
+    private
+
+    def handle_exception
+      log
+      capture_exception
+      handle_failure
+    end
+
+    def log
+      return unless Config.logger
+      message = [@message, @exception.message].compact.join('. ')
+      Config.logger.error message
+    end
+
+    def capture_exception
+      Config.capture_exception.call(@exception) if Config.capture_exception
+    end
+
+    def handle_failure
+      @on_failure.is_a?(Proc) ? @on_failure.call : @on_failure
+    end
   end
 end
